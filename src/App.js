e0e66d6fdf3be668f677cb2bdaa017d7e6dda7d4
shinji <shinbey221@gmail.com>
2019-10-04 17:50:10 +0900

    fix: Combine multiple states into object and set to useState

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+const App = props => {
+  const [state, setState] = useState(props)
+  const { name, price } = state
 
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-
-  const resetCount = () => setCount(0)
-
-  const twoTimes = () => setCount(count * 2)
-
-  const threeDivision = () => setCount(previousCount => (previousCount % 3 === 0) ? previousCount / 3 : previousCount)
-  
   return (
     <React.Fragment>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={resetCount}>Reset</button>
-        <button onClick={twoTimes}>x2</button>
-        <button onClick={threeDivision}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>現在の{name}は、{price}です</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})} />
     </React.Fragment>
   );
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
