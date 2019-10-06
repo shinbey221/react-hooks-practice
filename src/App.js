5bfd4aa5fcc26a13db1737ace580e1ab3cf51df0
shinji <shinbey221@gmail.com>
2019-10-06 11:34:03 +0900

    fix: implements useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = props => {
   const [state, setState] = useState(props)
   const { name, price } = state
+
+  useEffect (() => {
+    console.log('this is like componentDidMount')
+  }, [])
+
+  useEffect (() => {
+    console.log('name changed')
+  }, [name])
+
   return (
     <React.Fragment>
       <p>現在の{name}は、{price}です</p>
