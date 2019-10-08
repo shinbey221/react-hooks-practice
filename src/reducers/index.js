6138b144ab590275beadbe28ca5d3290eeded664
shinji <shinbey221@gmail.com>
2019-10-08 16:47:47 +0900

    fix: separate reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,4 @@
-import { CREATE_EVENT, DELETE_ALL_EVENT, DELETE_EVENT } from './../actions/index'
+import { combineReducers } from 'redux'
+import events from './events'
 
-const events = (state = [], action) => {
-  switch (action.type) {
-    case CREATE_EVENT:
-      const event = { title: action.title, body: action.body }
-      const length = state.length
-      let id
-      length === 0 ? id = 1 : id = state[length - 1].id + 1
-      return [...state, { id, ...event }]
-    case DELETE_EVENT:
-      return state.filter(item => item.id !== action.id)
-    case DELETE_ALL_EVENT:
-      return []
-    default:
-      return state
-  }
-}
-
-export default events
+export default combineReducers({ events })
